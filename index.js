2f696841a83993ca9819f6bf1731782092734490
kyrian <dr.kyrian.onwe@gmail.com>
2021-11-04 14:08:35 -0500

    commit tree cec8d22a4ca187cd12ed534cb78651ff999ac7f5

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer(){
+  globalThis.bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = "maybe bob";
+  return bestCustomer;
+}
+
+const leastFavoriteCustomer = "karen";
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "bob";
+  return leastFavoriteCustomer;
+}
